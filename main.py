17cb94a08d9af4a343704f29273df13430f9d1d8
anithacts67 <anitha.r264bac@cognizant.com>
2024-08-07 12:23:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
-from google.cloud import dialogflowcx_v3beta1
-from google.protobuf.json_format import MessageToJson
+import os
 import json
+from google.cloud import bigquery
+from google.cloud import storage
+import gcsfs
+import datetime
 
 
-
-def chat(input):
-    # Create a client
+def dialogflow_connect(request):
+    parameters_identified = False
+    request_json = request.get_json()
+    print(request_json)
+    #res = {"parameters": {"messages": [{"text": {"text": [text]}}]}}
     
-    client = dialogflowcx_v3beta1.SessionsClient(client_options={"api_endpoint": "us-central1-dialogflow.googleapis.com"})
-    
-    query_input = dialogflowcx_v3beta1.QueryInput()
-    query_input.text.text = input
-    query_input.language_code = "en"
-
-
-
-    request = dialogflowcx_v3beta1.DetectIntentRequest(
-        session="projects/cog01j2gvb54gvrcnk6c95qw452aw/locations/us-central1/agents/de99d644-2cb8-4618-9166-58c5a4902a7a/environments/Draft/sessions/*",
-        query_input=query_input,
-    )
-
-
-    # Making request
-    response = client.detect_intent(request=request)
-    print(response) # object as <class 'google.cloud.dialogflow_v2.types.DetectIntentResponse'>
-    response_json = MessageToJson(response._pb) # to json string
-    #print(response_json) 
-    response_dict= json.loads(response_json) # to convert into dict
-    #print(response_dict)
-    print(response_dict['queryResult']['responseMessages'][0]['text']['text'][0]) 
-    print(response_dict['queryResult']['intentDetectionConfidence']) 
-    print(response_dict['queryResult']['intent']['displayName'])
-    
-    # Parsing the response without coverting to json or dict
-    '''query_result = response.query_result
-    response_messages= query_result.response_messages 
-    intent = query_result.intent
-    display_name = intent.display_name
-    confidence = query_result.intent_detection_confidence
-  
-    print(f"response: {response_messages[0]}")
-    print(f"Intent: {display_name}")
-    print(f"Confidence: {confidence}")'''
-#calling chat function
-chat("hi")
+    tag = request_json["fulfillmentInfo"]["tag"]
+    if tag == "full info":
+        
+        #text= "please wait"
+        string_field_0=request_json['intentInfo']['parameters']['product']['resolvedValue']
+        #store_name=request_json['intentInfo']['parameters']['store_entries_en']['resolvedValue']
+        print(string_field_0)
+        #print(store_name)
+        # find other store identification parameters
+        client = bigquery.Client()
+    query = f"""
+       SELECT string_field_0, string_field_1, string_field_2 
+       FROM `cog01j2gvb54gvrcnk6c95qw452aw.demo_dataset_store.product_new` 
+       WHERE product_name = '{string_field_0}'
+    """
+    query_job = client.query(query)
+    result = query_job.result()
+    for row in result:
+        
+        text_response = f"Product {row.string_field_0} is located at aisle {row.string_field_2} in {row.string_field_1}."
+        
+   
+                  
+    result={"fulfillment_response": {"messages": [{"text": {"text": [text_response]}}]}}
+    return result
 
        
